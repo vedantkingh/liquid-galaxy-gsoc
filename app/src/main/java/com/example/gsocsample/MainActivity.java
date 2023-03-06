be7a8c77656b4a80c506db50f9aac83873a65e42
vedantkingh <vedantkingh@gmail.com>
2023-03-06 19:04:14 +0530

    Made App responsive.

--- app/src/main/java/com/example/gsocsample/MainActivity.java
+++ app/src/main/java/com/example/gsocsample/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.gsocsample;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +7,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
